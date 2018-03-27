605f78b86a955dc22a21112ccab54cb9a8f4f16d
Cameron Pickle <cmpickle@gmail.com>
2018-03-26 21:30:33 -0600

    password reset

--- final-project-cmpickle/Extensions/UrlHelperExtensions.cs
+++ final-project-cmpickle/Extensions/UrlHelperExtensions.cs
@@ -13,13 +13,13 @@ namespace Microsoft.AspNetCore.Mvc
                 protocol: scheme);
         }
 
-        // public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
-        // {
-        //     return urlHelper.Action(
-        //         action: nameof(AccountController.ResetPassword),
-        //         controller: "Account",
-        //         values: new { userId, code },
-        //         protocol: scheme);
-        // }
+        public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        {
+            return urlHelper.Action(
+                action: nameof(AccountController.ResetPassword),
+                controller: "Account",
+                values: new { userId, code },
+                protocol: scheme);
+        }
     }
 }
