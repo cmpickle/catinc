48fd0fa181da4c689aaf76fe25ddcbfbe6db1bf5
Cameron Pickle <cmpickle@gmail.com>
2018-04-04 23:27:38 -0600

    add MyUsers unit tests

---
+++ final-project-cmpickle.Tests/MyUsersTests.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using final_project_cmpickle.Models;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MyUsersTests
+    {
+        MyUsers user;
+
+        [SetUp]
+        public void Setup()
+        {
+            user = new MyUsers();
+        }
+
+        [Test]
+        public void SetUserIdTest()
+        {
+            user.UserID = 11;
+
+            Assert.That(user.UserID == 11);
+        }
+
+        [Test]
+        public void SetIdentityUserIdTest()
+        {
+            user.IdentityUserId = 12;
+
+            Assert.That(user.IdentityUserId == 12);
+        }
+
+        [Test]
+        public void SetIsUserDeletedTest()
+        {
+            user.IsUserDeleted = true;
+
+            Assert.That(user.IsUserDeleted == true);
+        }
+    }
+}
