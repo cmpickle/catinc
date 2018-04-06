786eae9ca0d015732af7ec225e678d6944b4af24
Cameron Pickle <cmpickle@gmail.com>
2018-04-05 19:55:48 -0600

    add vendor unit tests

--- final-project-cmpickle.Tests/Models/Domain/MyVendorTests.cs
+++ final-project-cmpickle.Tests/Models/Domain/MyVendorTests.cs
@@ -23,11 +23,75 @@ namespace Tests.Models.Domain
         }
 
         [Test]
-        public void SetVendor()
+        public void SetVendorName()
         {
             vendor.VendorName = "Cantina";
 
             Assert.That(vendor.VendorName.Equals("Cantina"));
+        }
+
+        [Test]
+        public void SetVendorAddress()
+        {
+            vendor.VendorAddress = "Mos Eisley";
+
+            Assert.That(vendor.VendorAddress.Equals("Mos Eisley"));
+        }
+
+        [Test]
+        public void SetVendorTelephoneNo()
+        {
+            vendor.VendorTelephoneNo = "555-555-5555";
+
+            Assert.That(vendor.VendorTelephoneNo.Equals("555-555-5555"));
+        }
+
+        [Test]
+        public void SetVendorEmail()
+        {
+            vendor.VendorEmail = "cantina@tatooine.rim";
+
+            Assert.That(vendor.VendorEmail.Equals("cantina@tatooine.rim"));
+        }
+
+        [Test]
+        public void SetVendorPaymentAmount()
+        {
+            vendor.VendorPaymentAmount = 345.56m;
+
+            Assert.That(vendor.VendorPaymentAmount == 345.56m);
+        }
+
+        [Test]
+        public void SetVendorCreditcardNo()
+        {
+            vendor.VendorCreditcardNo = 123456789;
+
+            Assert.That(vendor.VendorCreditcardNo == 123456789);
+        }
+
+        [Test]
+        public void SetVendorIsActive()
+        {
+            vendor.IsVendorActive = true;
+
+            Assert.That(vendor.IsVendorActive == true);
+        }
+
+        [Test]
+        public void SetVendorIsSuspended()
+        {
+            vendor.IsVendorSuspended = true;
+
+            Assert.That(vendor.IsVendorSuspended == true);
+        }
+
+        [Test]
+        public void setVendorIsDeleted()
+        {
+            vendor.IsvendorDeleted = true;
+
+            Assert.That(vendor.IsvendorDeleted);
         }
     }
 }
