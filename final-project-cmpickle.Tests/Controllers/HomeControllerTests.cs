b8a85e477a0cd277a32188eadb952712b6215f05
Cameron Pickle <cmpickle@gmail.com>
2018-04-06 23:06:46 -0600

    add homecontroller unit tests

---
+++ final-project-cmpickle.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using final_project_cmpickle.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private HomeController homeController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            homeController = new HomeController();
+        }
+
+        [Test]
+        public void RouteToIndex()
+        {
+            Assert.That(typeof(IActionResult).IsInstanceOfType(homeController.Index()));
+        }
+
+        [Test]
+        public void RouteToAbout()
+        {
+            Assert.That(typeof(IActionResult).IsInstanceOfType(homeController.About()));
+        }
+
+        [Test]
+        public void RouteToContact()
+        {
+            Assert.That(typeof(IActionResult).IsInstanceOfType(homeController.Contact()));
+        }
+    }
+}
