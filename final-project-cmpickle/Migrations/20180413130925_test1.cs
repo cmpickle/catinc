0df893c526738f9f7a380a2c2637c4b523ec4ad9
Cameron Pickle <cmpickle@gmail.com>
2018-04-13 07:11:03 -0600

    new migrations

---
+++ final-project-cmpickle/Migrations/20180413130925_test1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace finalprojectcmpickle.Migrations
+{
+    public partial class test1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "VendorID",
+                table: "Vendor",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int))
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "VendorID",
+                table: "Vendor",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldDefaultValue: 1)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
