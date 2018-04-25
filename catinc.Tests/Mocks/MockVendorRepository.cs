5b8ea3a76a3885eba8676dcfe3e3746669fd97d1
Cameron Pickle <cmpickle@gmail.com>
2018-04-25 06:33:21 -0600

    all unit tests working

--- catinc.Tests/Mocks/MockVendorRepository.cs
+++ catinc.Tests/Mocks/MockVendorRepository.cs
@@ -2,14 +2,21 @@ using catinc.Repositories;
 using catinc.Models.Database;
 using catinc.Models.Domain;
 using catinc.Models.MemberSystem;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace catinc.Tests.Mocks
 {
     public class MockVendorRepository : VendorRepository
     {
+        static DbContextOptions<MySqlDbContext> options = new DbContextOptionsBuilder<MySqlDbContext>()
+            .UseInMemoryDatabase(databaseName: "cmpickle")
+            .Options;
+
+        static MySqlDbContext context = new MySqlDbContext(options);
+
         public MockVendorRepository() 
-            : base(new Mock<MySqlDbContext>().Object,
+            : base(context,
                     new Mock<IUserManager<MyIdentityUser>>().Object)
         {}
     }
