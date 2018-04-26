480a760cee41fd7d960815984873b0c216c40cd4
Cameron Pickle <cmpickle@gmail.com>
2018-04-26 06:05:01 -0600

    looking at authorization

--- catinc/Startup.cs
+++ catinc/Startup.cs
@@ -62,7 +62,7 @@ namespace catinc
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -84,6 +84,47 @@ namespace catinc
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            // CreateRoles(serviceProvider).Wait();
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            //adding custom roles
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<MyIdentityUser>>();
+            string[] roleNames = { "Admin", "Manager", "User" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                //creating the roles and seeding them to the database
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            //creating a super user who could maintain the web app
+            var poweruser = new MyIdentityUser
+            {
+                UserName = Configuration.GetSection("UserSettings")["UserEmail"],
+                Email = Configuration.GetSection("UserSettings")["UserEmail"]
+            };
+
+            string UserPassword = Configuration.GetSection("UserSettings")["UserPassword"];
+            var _user = await UserManager.FindByEmailAsync(Configuration.GetSection("UserSettings")["UserEmail"]);
+
+            if(_user == null)
+            {
+                    var createPowerUser = await UserManager.CreateAsync(poweruser, UserPassword);
+                    if (createPowerUser.Succeeded)
+                    {
+                        //here we tie the new user to the "Admin" role 
+                        await UserManager.AddToRoleAsync(poweruser, "Admin");
+
+                    }
+            }
         }
     }
 }
