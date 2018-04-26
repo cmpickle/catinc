00ac88473fe219f421c6b7d2b6ba444d8bb91d0a
Cameron Pickle <cmpickle@gmail.com>
2018-04-25 21:33:43 -0600

    more products, vendors, and fixed style problem

--- catinc/Models/Database/SeedData.cs
+++ catinc/Models/Database/SeedData.cs
@@ -16,6 +16,7 @@ namespace catinc.Models.Database
                 Vendor flatlanders = null;
                 Vendor krustykrab = null;
                 Vendor carmicheal = null;
+                Vendor psych = null;
                 if (!context.Vendors.Any())
                 {
                     context.Vendors.AddRange(
@@ -33,7 +34,15 @@ namespace catinc.Models.Database
                             VendorEmail = "money@krustykrab.web",
                             VendorAddress = "1 Bikini Bottom Ave",
                             VendorTelephoneNo = "9015555555",
-                            VendorCreditcardNo = 123456780
+                            VendorCreditcardNo = 123456781
+                        },
+                        psych = new Vendor
+                        {
+                            VendorName = "Psych",
+                            VendorEmail = "cmon@son.interweb",
+                            VendorAddress = "100 Oceanside Ave",
+                            VendorTelephoneNo = "9015555555",
+                            VendorCreditcardNo = 123456782
                         },
                         carmicheal = new Vendor
                         {
@@ -41,7 +50,7 @@ namespace catinc.Models.Database
                             VendorEmail = "intersect@carmicheal.com",
                             VendorAddress = "1257 Buymoria Burbank CA",
                             VendorTelephoneNo = "9025555555",
-                            VendorCreditcardNo = 123456781
+                            VendorCreditcardNo = 123456780
                         }
                     );
                 }
@@ -85,6 +94,42 @@ namespace catinc.Models.Database
                             SecurityStamp = "686334eb-18a2-44e8-9758-0529266883f4",
                             TwoFactorEnabled = false,
                             UserName = "test@test.com"
+                        },
+                        garbage = new MyIdentityUser
+                        {
+                            Id = "2c84da0e-2134-44e4-b6aa-8a88f035d846",
+                            AccessFailedCount = 0,
+                            ConcurrencyStamp = "6d140b68-af83-4b56-b692-2161af80500e",
+                            Email = "test1@test.com",
+                            EmailConfirmed = false,
+                            LockoutEnabled = true,
+                            LockoutEnd = null,
+                            NormalizedEmail = "TEST1@TEST.COM",
+                            NormalizedUserName = "TEST1@TEST.COM",
+                            PasswordHash = "AQAAAAEAACcQAAAAECHsyeaF0iFuahlYZFJXpbaibNLCMomnVCySupwES3zuXHYD/jzn80QNIZFVVrTPlw==",
+                            PhoneNumber = null,
+                            PhoneNumberConfirmed = false,
+                            SecurityStamp = "33ae9241-eee8-41ab-97a7-62ef99813834",
+                            TwoFactorEnabled = false,
+                            UserName = "test1@test.com"
+                        },
+                        garbage = new MyIdentityUser
+                        {
+                            Id = "6897bcee-59be-45d4-aafc-8e137702c1d6",
+                            AccessFailedCount = 0,
+                            ConcurrencyStamp = "0783360d-2973-40fa-b448-64fa5b7dc231",
+                            Email = "test2@test.com",
+                            EmailConfirmed = false,
+                            LockoutEnabled = true,
+                            LockoutEnd = null,
+                            NormalizedEmail = "TEST2@TEST.COM",
+                            NormalizedUserName = "TEST2@TEST.COM",
+                            PasswordHash = "AQAAAAEAACcQAAAAENVEXH1hweYJRlsbI1/cb3DrvvK1P8pDCmh2S1HVQTngE/VtphjoFp13/WDtOIee1w==",
+                            PhoneNumber = null,
+                            PhoneNumberConfirmed = false,
+                            SecurityStamp = "bc06cab4-e05c-410d-8d4f-c81b7c79c3c8",
+                            TwoFactorEnabled = false,
+                            UserName = "test2@test.com"
                         }
                     );
                 }
@@ -96,6 +141,21 @@ namespace catinc.Models.Database
                         {
                             Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Carmicheal Industries")),
                             User = context.MyIdentityUsers.FirstOrDefault(u => u.UserName.Equals("parathegarbage@gmail.com")),
+                        },
+                        new VendorUser
+                        {
+                            Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Psych")),
+                            User = context.MyIdentityUsers.FirstOrDefault(u => u.UserName.Equals("test1@test.com")),
+                        },
+                        new VendorUser
+                        {
+                            Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Flatlanders")),
+                            User = context.MyIdentityUsers.FirstOrDefault(u => u.UserName.Equals("test2@test.com")),
+                        },
+                        new VendorUser
+                        {
+                            Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Krusty Krab")),
+                            User = context.MyIdentityUsers.FirstOrDefault(u => u.UserName.Equals("test@test.com")),
                         }
                     );
                }
@@ -135,6 +195,83 @@ namespace catinc.Models.Database
                             ProductInventory = 200,
                             ProductImageURL = "/images/gem-03.gif",
                             Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Flatlanders")),
+                        },
+                        new Product
+                        {
+                            ProductSKU = "1237",
+                            ProductName = "Nerd Herd Service",
+                            ProductShortDescription = "Best service you can buy.",
+                            ProductLongDescription = "The Buymore's service that will cover you on any of your home electronics needs.",
+                            ProductPrice = 200.00M,
+                            ProductInventory = 999,
+                            ProductImageURL = "/images/nerdherd.png",
+                            Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Carmicheal Industries")),
+                        },
+                        new Product
+                        {
+                            ProductSKU = "1238",
+                            ProductName = "Intersect Glasses",
+                            ProductShortDescription = "Government secrets in glasses.",
+                            ProductLongDescription = "Become a super spy with all of the goverments secrets stored in your head.",
+                            ProductPrice = 10000.00M,
+                            ProductInventory = 2,
+                            ProductImageURL = "/images/intersect.jpg",
+                            Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Carmicheal Industries")),
+                        },
+                        new Product
+                        {
+                            ProductSKU = "1239",
+                            ProductName = "Pineapple",
+                            ProductShortDescription = "A fresh Pineapple.",
+                            ProductLongDescription = "A fresh California pineapple! You might even convince Shawn to cut it for you if your are peckish enough.",
+                            ProductPrice = 10.00M,
+                            ProductInventory = 30,
+                            ProductImageURL = "/images/pineapple.jpg",
+                            Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Psych")),
+                        },
+                        new Product
+                        {
+                            ProductSKU = "1240",
+                            ProductName = "Blueberry",
+                            ProductShortDescription = "The Blueberry in all it's glory.",
+                            ProductLongDescription = "You may not be a gifted psychic detective but you can drive around like one!",
+                            ProductPrice = 15000.00M,
+                            ProductInventory = 5,
+                            ProductImageURL = "/images/blueberry.jpg",
+                            Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Psych")),
+                        },
+                        new Product
+                        {
+                            ProductSKU = "1241",
+                            ProductName = "Krabby Patty",
+                            ProductShortDescription = "Finest Sandwich in the sea.",
+                            ProductLongDescription = "This sanwich will make your sould delight when you sample it's greatness.",
+                            ProductPrice = 5.00M,
+                            ProductInventory = 50,
+                            ProductImageURL = "/images/krabbypatty.jpg",
+                            Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Krusty Krab")),
+                        },
+                        new Product
+                        {
+                            ProductSKU = "1242",
+                            ProductName = "Krust Krab Pizza",
+                            ProductShortDescription = "Is the Pizza",
+                            ProductLongDescription = "The Krusty Krab Pizza is the pizza for you and me!",
+                            ProductPrice = 10.00M,
+                            ProductInventory = 20,
+                            ProductImageURL = "/images/krustykrabpizza.png",
+                            Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Krusty Krab")),
+                        },
+                        new Product
+                        {
+                            ProductSKU = "1243",
+                            ProductName = "Jelly Fish Jelly",
+                            ProductShortDescription = "Made from real jellyfish.",
+                            ProductLongDescription = "Made from real jellyfish you can put it on your Krabby Patty.",
+                            ProductPrice = 10.00M,
+                            ProductInventory = 35,
+                            ProductImageURL = "/images/jellyfishjelly.jpg",
+                            Vendor = context.Vendors.FirstOrDefault(v => v.VendorName.Equals("Krusty Krab")),
                         }
                     );
                }
